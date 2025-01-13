8ba4ae91b770105dfa84014ec54ba43268155fc9
Celeste Cruz <celesteglezcruz@gmail.com>
2025-01-13 15:32:41 -0500

    añadiendo router-dom en frontend

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,15 @@
-import { RouteObject } from "react-router-dom";
+import { Navigate, RouteObject } from "react-router-dom";
 import Login from "./pages/auth/login/Login";
 
 const appRoutes: RouteObject[] = [
   //Ruta principal
   {
     path: '/',
-    element: <Login to="/login" replace />
+    element: <Navigate to="/login" replace />
+  },
+  {
+    path: '/login',
+    element: <Login />
   }
 ]
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppRoutes.tsx'
+import appRoutes from './AppRoutes.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(appRoutes)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
